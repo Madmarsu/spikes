4b43bf52b8230015594e934f0be595446631b368
Madmarsu <madmarsu@gmail.com>
2017-06-26 17:51:17 +0200

    draft

---
+++ module_struct.js
@@ -0,0 +1,14 @@
+
+/* fichier décrivant la structure des données utilisées pour un "module"
+
+class structModule {
+  constructor(id) {
+    this.id: id;
+    this.x, this.y, this.w, this.h: 0;
+    this.type_module: "type module";
+    this.titre: "nom du module";
+    this.open: true;
+    this.openDate: now();
+    this.duree_finale: 0;
+  }
+}
